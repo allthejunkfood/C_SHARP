c7670bc89199034aae6ffbda05816c1e2209e3a8
ChiefBitCounter <rconley6836@comcast.net>
2020-04-24 14:46:31 -0400

    basic cSharp

---
+++ Basic C-Sharp Projects/AcmeAccountingSystems/AcmeAccountingSystems/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AcmeAccountingSystems
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Solutions");
+            Console.WriteLine("Remember, we're "+"accounting"+" on you!");
+            Console.ReadLine();
+        }
+    }
+}
